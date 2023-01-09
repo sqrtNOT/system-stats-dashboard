d0189a355bbdeef95534226d98ca1f876f59a38e
sqrtNOT <77981959+sqrtNOT@users.noreply.github.com>
2023-01-09 07:58:51 +0000

    black

--- dags/sensors_dag.py
+++ dags/sensors_dag.py
@@ -3,20 +3,20 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 
 with DAG(
-	'Temps',
-	default_args={
-		'depends_on_past': False,
-		'retries': 0,
-	},
-	description='Parse the sensors command and store it in mysql',
-	schedule_interval='* * * * *', #every minute
-	start_date=datetime(2022, 6, 4),
-	catchup=False,
+    "Temps",
+    default_args={
+        "depends_on_past": False,
+        "retries": 0,
+    },
+    description="Parse the sensors command and store it in mysql",
+    schedule_interval="* * * * *",  # every minute
+    start_date=datetime(2022, 6, 4),
+    catchup=False,
 ) as dag:
-	get_temps = BashOperator(
-		task_id='get_temps',
-		bash_command="sensors.sh ",
-)
+    get_temps = BashOperator(
+        task_id="get_temps",
+        bash_command="sensors.sh ",
+    )
 
 
 get_temps
